71c5f147612da8bcc9585de502f428c2035e97c0
Vasilisa Ponomarenko <ponoma@berkeley.edu>
2024-05-30 17:17:30 -0700

    Finished encoder. Need to implement masked self attention

--- model.py
+++ model.py
@@ -10,20 +10,24 @@ class Encoder(nn.Module):
         self.k = nn.Linear(d_model, d_model)        # if implementing multiheaded attention later, good to have these here to use in both mutltiheaded and self-attention classes
         self.v = nn.Linear(d_model, d_model)
         self.attention = Self_Attention()     # need to initialize before running forward
-        self.norm1 = nn.LayerNorm(d_model)
+        self.dropout1 = nn.Dropout(0.1)
+        self.add_and_norm1 = Add_and_Norm(d_model)      # TODO: should I implement Add_and_Norm class separately?
 
-        self.ffn = Position_wise_ffn(d_model, d_ffn)
-        self.norm2 = nn.LayerNorm(d_model)
+        self.ffn = Position_wise_ffn(d_model, d_ffn)    # output has dimension d_model
+        self.dropout2 = nn.Dropout(0.1)
+        self.add_and_norm2 = Add_and_Norm(d_model)
 
     def forward(self, q, k, v, x):
         Q = self.q(q)
         K = self.k(k)
         V = self.v(v)
-        attention = attention(Q, K, V) 
-        attention = self.norm(attention + x)        # x is original input embedding
+        attention = attention(Q, K, V)
+        self.dropout1(attention) 
+        x = self.add_and_norm1(attention, x)    # x is original input embedding
 
-        ffn = self.ffn(attention)
-        ffn = self.norm2(ffn + attention)
+        ffn = self.ffn(x)
+        ffn = self.dropout2(ffn)
+        ffn = self.add_and_norm2(x, ffn)
         return ffn
 
 class Decoder(nn.Module):
@@ -36,25 +40,34 @@ class Decoder(nn.Module):
 
 # Helper classes
 
-class Position_wise_ffn(nn.Module):
-    def __init__(self, d_model, d_ffn):
+class Position_wise_ffn(nn.Module):         # 2 fully connected dense layers  https://medium.com/@hunter-j-phillips/position-wise-feed-forward-network-ffn-d4cc9e997b4c 
+    def __init__(self, d_model, d_ffn):     # feed forward just means no recurrent relations
         super().__init__()
         self.linear1 = nn.Linear(d_model, d_ffn)
         self.linear2 = nn.Linear(d_ffn, d_model)
     
     def forward(self, x):
-        x = self.linear1(x)         # TODO add and norm
+        x = self.linear1(x)         
         x = torch.relu(x)
         x = self.linear2(x)
         return x
 
 
-class Self_Attention():            # q and k have dimensions d_v by d_k
+class Self_Attention(nn.Module):            # q and k have dimensions d_v by d_k
     def __init__(self):
-        super().__init__()
+        super().__init__()          # TODO: implement mask option
 
     def forward(self, q, k, v):
         d_k = q.size(-1)                    # get last dimension of q (should be d_k)
-        attention_weights = torch.matmul(q, k.transpose(-2, -1)) / torch.sqrt(d_k)
+        attention_weights = torch.matmul(q, k.transpose(-2, -1)) / torch.sqrt(d_k)      # want last two dimensions to get swapped
         probabilities = torch.softmax(attention_weights, dim=-1)  # gets the probabilities along last dimension. For 2d the result of softmax is a d_v by 1 vector.
         return torch.matmul(probabilities, v)  # multiply probabilities with v to get the weighted sum of v. 
+    
+
+class Add_and_Norm(nn.Module):
+    def __init__(self, d_model):
+        super().__init__()
+        self.norm = nn.LayerNorm(d_model)
+
+    def forward(self, input, output):
+        return self.norm(input + output)
