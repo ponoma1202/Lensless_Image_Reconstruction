23f3846e858874ea2397bde60f9c920ad3266164
Vasilisa Ponomarenko <ponoma1202@gmail.com>
2024-06-13 14:32:21 -0700

    Modified training so that validation and train_epoch are run every epoch. Starting to configure wandb

--- train.py
+++ train.py
@@ -4,16 +4,26 @@ import torchvision
 from utils import Rescale
 from tqdm import tqdm
 import os
+import wandb
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID" 
 os.environ["CUDA_VISIBLE_DEVICES"] = '0,1,2'
-gpu_number = 1
+gpu_number = 0
 
 def main():
-    # Use CIFAR 10 for the data
-    batch_size = 32
+    # Using CIFAR 10 for the data
+
+    batch_size = 64
     num_epochs = 10
+    learning_rate = 1e-4
+    num_classes = 10
     save_path = './checkpoint/model.pth'
+    wandb.init(project='basic_transformer', config={"learning_rate":learning_rate,
+                                                    "architecture": Transformer,
+                                                    "dataset": 'CIFAR-10',
+                                                    "epochs":num_epochs,
+                                                    "batch_size":batch_size,
+                                                    "classes":num_classes})
 
     transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor(),                  # issue: scales image to [0,1] range          
                                                 # do not want to center around 0. center around mean instead to avoid negative values (for embedding)
@@ -22,13 +32,15 @@ def main():
                                                 Rescale()])      # resolve scaling issue from ToTensor (embedding only takes in ints, so we need [0, 255] range)      
                                           
     trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=False, transform=transform)
+
+    # TODO: question about setting num_workers parameter in dataloader (what should it be set to?)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True)
 
     testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=False, transform=transform)
     testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False)
 
-    in_dim = 256                # Number of embeddings in input sequence (size of input vocabulary). Pixels have range [0, 255]
-    out_dim = 10                # Number of embeddings in output sequence. Each image is labeled with a single class (10 total classes)
+    in_dim = 256                        # Number of embeddings in input sequence (size of input vocabulary). Pixels have range [0, 255]
+    out_dim = num_classes               # Number of embeddings in output sequence. Each image is labeled with a single class (10 total classes)
     
     # See if gpu is available
     if torch.cuda.is_available():
@@ -41,40 +53,46 @@ def main():
     model = Transformer(in_dim, out_dim, device)
     model.to(device)
 
-    learning_rate = 1e-5
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)           # TODO: potentially add learning rate scheduler later and change parameters to paper's params
     criterion = torch.nn.CrossEntropyLoss()
 
     if not os.path.exists(save_path):          
         os.mkdir(save_path)
 
-    train(model, trainloader, optimizer, criterion, num_epochs, device, save_path)
-    validate(model, testloader, criterion, device, save_path)
+    train(model, trainloader, testloader, optimizer, criterion, num_epochs, device, save_path)
 
-
-def train(model, trainloader, optimizer, criterion, num_epochs, device, save_path):
-    model.train()
-    
+def train(model, trainloader, testloader, optimizer, criterion, num_epochs, device, save_path):
     for epoch in range(num_epochs):
-        total_correct = 0
-        total = 0
-        for step, batch in enumerate(tqdm(trainloader)):
-            input, target = batch
-            input, target = input.to(device), target.to(device)
-            output = model(input, target)                                                    # result is a (num_classes, batch_size) tensor
-            optimizer.zero_grad()
-            loss = criterion(output.squeeze(), target)                                       # take argmax to get the class with the highest "probability"
-            loss.backward()
-            optimizer.step() 
-            pred = output.squeeze().argmax(dim=1)                                       # output is (batch_size, target seq len, num_classes) so need to squeeze to (batch_size, num_classes). For classification, target seq len = 1
-            total += target.size(0)                             # get batch size
-            total_correct += (pred == target).sum().item()                              # summing over a list results in a list so need to use .item() to get a number.
-        print("Total number correct: ", total_correct)
-        print("Total number of images", len(trainloader.dataset))
-        print(f'Epoch {epoch+1}/{num_epochs}, Loss: {loss.item()}, Accuracy: {total_correct / len(trainloader.dataset)}')
-    
-    torch.save(model.state_dict(), save_path)         
+        print(f'Start training epoch {epoch}/{num_epochs}...')
+        train_accuracy, train_loss = train_epoch(model, epoch, num_epochs, trainloader, optimizer, criterion, device) 
+        val_acc, val_loss = validate(model, testloader, criterion, device, save_path)
+        wandb.log({"training_accuracy":train_accuracy, "training_loss":train_loss, "validation_acc":val_acc, "validation_loss":val_loss, "epoch":epoch})
+    torch.save(model.state_dict(), save_path)
+        
+# TODO: average losses over all iterations
+def train_epoch(model, epoch, num_epochs, trainloader, optimizer, criterion, device):
+    model.train()
+    total_correct = 0
+    total_loss = 0
 
+    for step, batch in enumerate(tqdm(trainloader)):
+        input, target = batch
+        input, target = input.to(device), target.to(device)
+        output = model(input, target)                                                    # result is a (num_classes, batch_size) tensor
+        optimizer.zero_grad()
+        loss = criterion(output.squeeze(), target)                                       # take argmax to get the class with the highest "probability"
+        loss.backward()
+        optimizer.step() 
+        pred = output.squeeze().argmax(dim=1)                                       # output is (batch_size, target seq len, num_classes) so need to squeeze to (batch_size, num_classes). For classification, target seq len = 1                             # get batch size
+        total_loss += loss.item()
+        total_correct += (pred == target).sum().item()                              # summing over a list results in a list so need to use .item() to get a number.
+    accuracy = total_correct / len(trainloader.dataset)
+    avg_loss = total_loss/ len(trainloader.dataset)
+    print(f'Epoch {epoch+1}/{num_epochs}, Loss: {avg_loss}, Accuracy: {accuracy}')
+    return accuracy, avg_loss          
+
+
+# TODO: aggregate and average loss over all iterations
 def validate(model, testloader, criterion, device, save_path, load=False):
     if load:
         model.load_state_dict(torch.load(save_path))                         # if loading from saved model
@@ -83,14 +101,20 @@ def validate(model, testloader, criterion, device, save_path, load=False):
     print("Starting validation...")
     with torch.no_grad():
         total_correct = 0
+        total_loss = 0.0
+
         for step, batch in enumerate(tqdm(testloader)):
             input, target = batch
             input, target = input.to(device), target.to(device)
             output = model(input, target)
             loss = criterion(output.squeeze(), target)                                  # Need to .squeeze() because of headed attention.
             pred = output.squeeze().argmax(dim=1)
+            total_loss += loss
             total_correct += (pred == target).sum().item()
-        print(f'Validation Loss: {loss.item()}, Validation Accuracy: {total_correct/len(testloader.dataset)}')
+        accuracy = total_correct/len(testloader.dataset)
+        avg_loss = total_loss/len(testloader.dataset)
+        print(f'Validation Loss: {avg_loss}, Validation Accuracy: {accuracy}')
+        return accuracy, avg_loss
     
 
 if __name__ == "__main__":
